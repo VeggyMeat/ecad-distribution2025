02ca950d9d43e9ccc375637b54aef525a6de3f3a
VeggyMeat <atrm2@cam.ac.uk>
2025-10-13 20:35:36 +0100

    Moved to the other algorithm

--- riscv/assembly/src/div.s
+++ riscv/assembly/src/div.s
@@ -14,17 +14,39 @@ div:
     # ...
 
     # do your work
-    li t0, 0
-    beq a1, zero, exit  # if divisor is 0, return 0
-loop:
-    sub a0, a0, a1  # a0 = a0 - a1
-    blt t0, t1, exit 
-    add t0, t0, 1 # t0 = t0 + 1
-    beq a0, a0, loop
-exit: 
-    lw a1, 0(a0) # load remainder
-    lw a0, 0(t0) # load quotient
+    li t0, 0 # number of bits in N
+    mv t1, a0 # load dividend
+    li t2, 0 # 0
+    beq t1, t2, done_0 # if the dividend is 0 finish now
+    beq a1, t2, done_0 # if the divisor is 0 finish now
+count_length:
+    addi t0, t0, 1 # add one to the number of bits
+    srli t1, t1, 1 # half the dividend
+    bne t1, t2, count_length # if dividend equal to 0 we've got the right count
 
+    li t1, 0 # Q = 0
+    li t2, 0 # R = 0
+    li t3, 1 # 1
+    li t4, -1 # -1
+    srl t6, t3, t0 # mask equal to 1 with N 0s
+loop:
+    sub t0, t0, t3 # i -= 1
+    beq t0, t4, done # if i == -1 then we're done
+    slli t2, t2, 1 # R := R << 1
+    slli t6, t6, 1 # mask right shifted one spot
+    and t5, t6, a0 # get the ith bit of N
+    srl t5, t5, t0 # shift it to the least significant bit
+    add t2, t2, a5 # R(0) := R + N(i)
+    blt t2, a1, loop # if R < D then continue
+    sub t2, t2, a1 # R := R - D
+    add t1, t1, t6 # Q(i) := 1
+    beq t0, t0, loop # loop
+done_0:
+    li t1, 0 # Q = 0
+    li t2, 0 # R = 0
+done:
+    mv a0, t1 # load return value Q
+    mv a1, t2 # load return value R
     # load every register you stored above
     lw   ra, 28(sp)
     lw   s0, 24(sp)
